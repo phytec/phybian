5022a0cae477014285f395e010fd4609975d37f1
Stefan Müller-Klieser <s.mueller-klieser@phytec.de>
2023-03-09 12:04:53 +0100

    init: update commands
    With latest changes to the build_system variable and building on podman,
    we have very simple build commands.
    
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/siemens/kas
 export DL_DIR=$(pwd)/dl_dir
-export KAS_ALLOW_ROOT=yes
 kas/run-kas checkout phytec_polis.yml
-kas/kas-docker --isar build phytec_polis.yml
+kas/kas-container build phytec_polis.yml
